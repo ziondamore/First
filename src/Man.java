6e6338701dbbc3e1ede940f6626c49f8455cff65
ZionDamore <wongotgc@gmail.com>
2019-07-10 19:37:46 +0300

    Refactoring code

---
+++ src/Man.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Man {
+        public int indexNumber;
+        public String name;
+        public int iq;
+
+        public Man(int indexNumber, String name, int iq) {
+            this.name = name;
+            this.indexNumber = indexNumber;
+            this.iq = iq;
+        }
+        public String toString() {
+            return this.indexNumber + " " + this.name + " " + this.iq;
+        }
+
+}
+class  Sort implements Comparator<Man> {
+   public int compare(Man a, Man b) {
+       return b.iq - a.iq;
+   }
+}
+class MenBubble {
+    public static void main(String[] args) {
+        Man[] listMen = {
+                new Man(1, "Alex", 125),
+                new Man(2, "Vlad", 121),
+                new Man(3, "Kolya", 88),
+                new Man(4, "Tima", 133),
+                new Man(5, "Vova", 198),
+                new Man(6, "Victor", 142),
+                new Man(7, "Dimon", 112),
+                new Man(8, "Oleg", 119),
+                new Man(9, "Gleb", 85),
+                new Man(10, "Artem", 133),
+                new Man(11, "Goga", 146),
+                new Man(12, "Kostya", 78),
+                new Man(13, "Vadim", 89),
+                new Man(14, "Alexander", 165),
+                new Man(15, "Garic", 156),
+                new Man(16, "Grisha", 130),
+                new Man(17, "Sergey", 129),
+                new Man(18, "Stas", 135),
+                new Man(19, "Gosha", 79),
+                new Man(20, "Albert", 96),
+        };
+
+        System.out.println("\nUnsorted");
+        for (int i = 0; i < listMen.length; i++) {
+            System.out.println(listMen[i]);
+        }
+        Arrays.sort(listMen, new Sort());
+
+
+        System.out.println("\nSorted by IQ: Top 10");
+        Man[] getCopy_Man = Arrays.copyOfRange(listMen, 0, 10);
+        for (Man listMen1 : getCopy_Man) {
+            System.out.println(listMen1.toString() + " ");
+            System.out.println();
+        }
+    }
+}
