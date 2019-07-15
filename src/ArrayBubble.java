84bab416a534eeb0d9963b3e305a4ebe956d13e9
ZionDamore <wongotgc@gmail.com>
2019-07-15 12:19:46 +0300

    add ArrayList

--- src/ArrayBubble.java
+++ src/ArrayBubble.java
@@ -1,28 +1,32 @@
+import java.util.ArrayList;
+
 class ArrayBubble {
     public static void main(String[] args) {
         int top = 10;
-        Man[] listMen = {
-                new Man(1, "Alex", 125),
-                new Man(2, "Vlad", 121),
-                new Man(3, "Kolya", 88),
-                new Man(4, "Tima", 133),
-                new Man(5, "Vova", 198),
-                new Man(6, "Victor", 142),
-                new Man(7, "Dimon", 112),
-                new Man(8, "Oleg", 119),
-                new Man(9, "Gleb", 85),
-                new Man(10, "Artem", 133),
-                new Man(11, "Goga", 146),
-                new Man(12, "Kostya", 78),
-                new Man(13, "Vadim", 89),
-                new Man(14, "Alexander", 165),
-                new Man(15, "Garic", 156),
-                new Man(16, "Grisha", 130),
-                new Man(17, "Sergey", 129),
-                new Man(18, "Stas", 135),
-                new Man(19, "Gosha", 79),
-                new Man(20, "Albert", 96),
-        };
+
+        ArrayList<Man> listMen = new ArrayList<>();
+
+        listMen.add(new Man(1, "Alex", 125));
+        listMen.add(new Man(2, "Vlad", 121));
+        listMen.add(new Man(3, "Kolya", 88));
+        listMen.add(new Man(4, "Tima", 133));
+        listMen.add(new Man(5, "Vova", 198));
+        listMen.add(new Man(6, "Victor", 142));
+        listMen.add(new Man(7, "Dimon", 112));
+        listMen.add(new Man(8, "Oleg", 119));
+        listMen.add(new Man(9, "Gleb", 85));
+        listMen.add(new Man(10, "Artem", 133));
+        listMen.add(new Man(11, "Goga", 146));
+        listMen.add(new Man(12, "Kostya", 78));
+        listMen.add(new Man(13, "Vadim", 89));
+        listMen.add(new Man(14, "Alexander", 165));
+        listMen.add(new Man(15, "Garic", 156));
+        listMen.add(new Man(16, "Grisha", 130));
+        listMen.add(new Man(17, "Sergey", 129));
+        listMen.add(new Man(18, "Stas", 135));
+        listMen.add(new Man(19, "Gosha", 79));
+        listMen.add(new Man(20, "Albert", 96));
+
 
         System.out.println("\nUnsorted");
         printMen(listMen);
@@ -30,18 +34,18 @@ class ArrayBubble {
 
         System.out.println("\nSorted Top " + top);
         for (int i = 0; i < top; i++) {
-            System.out.println(listMen[i]);
+            System.out.println(listMen.get(i));
         }
     }
 
-    private static void printMen(Man[] listMen) {
-        for (int i = 0; i < listMen.length; i++) {
-            System.out.println(listMen[i]);
+    private static void printMen(ArrayList<Man> listMen) {
+        for (int i = 0; i < listMen.size(); i++) {
+            System.out.println(listMen.get(i));
         }
     }
 
-    private static void sortMenByIQWithBubbleSort(Man[] source) {
-        int length = source.length;
+    private static void sortMenByIQWithBubbleSort(ArrayList<Man> source) {
+        int length = source.size();
         boolean swapped;
         Man temporaryVariable;
 
@@ -49,10 +53,10 @@ class ArrayBubble {
             swapped = false;
 
             for (int j = 0; j < length - i - 1; j++) {
-                if (source[j].iq < source[j + 1].iq) {
-                    temporaryVariable = source[j];
-                    source[j] = source[j + 1];
-                    source[j + 1] = temporaryVariable;
+                if (source.get(j).iq < source.get(j + 1).iq) {
+                    temporaryVariable = source.get(j);
+                    source.set(j, (source.get(j +1)));
+                    source.set((j + 1),temporaryVariable);
                     swapped = true;
                 }
             }
